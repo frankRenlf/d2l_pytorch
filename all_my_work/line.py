c835b2cdadb518dfd22db55d4e71b059ed9e9d42
frankRenlf <frank.renlingfeng@gmail.com>
2023-10-23 19:51:40 +0800

    update

--- all_my_work/line.py
+++ all_my_work/line.py
@@ -9,11 +9,30 @@
     @Description : 
 """
 import numpy as np
+import torch.nn as nn
+import torch
+
+
+def pseudo_A(A, b):
+    # Ax = b   =>   x = (A.T @ A)^-1 @ A @ b
+    if b is None:
+        P = A @ np.linalg.inv(A.T @ A) @ A.T
+        print(P)
+    else:
+        x = np.linalg.inv(A.T @ A) @ A.T @ b
+        print(x)
+        print(A @ x)
+
+
+def orthogonal():
+    A = np.array([[1], [1], [1]], dtype=np.float32)
+    b = np.array([[1], [0], [2]])
+    # Ax = b   =>   x = (A.T @ A)^-1 @ A @ b
+    B = b - A @ (A.T @ b / (A.T @ A))
+    print(B)
+
 
 if __name__ == "__main__":
-    A = np.matrix([[1, 1], [1, 2], [1, 3]])
-    b = np.matrix([[1], [2], [2]])
-    # Ax = b   =>   x = (A.T @ A)^-1 @ A @ b
-    x = np.linalg.inv(A.T @ A) @ A.T @ b
-    print(x)
-    print(A @ x)
+    pseudo_A(A=np.array([[2], [1], [2]]), b=None)
+    # orthogonal()
+    # print(np.__version__)
