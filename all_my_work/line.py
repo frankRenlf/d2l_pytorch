eb1409873d0459d1c9eaa4aac2b884f374c39088
frankRenlf <frank.renlingfeng@gmail.com>
2023-10-27 14:58:42 +0800

    update

--- all_my_work/line.py
+++ all_my_work/line.py
@@ -16,10 +16,11 @@ import torch
 def pseudo_A(A, b, u0):
     # Ax = b   =>   x = (A.T @ A)^-1 @ A @ b
     if b is None:
+        # print(A.T @ A)
         P = A @ np.linalg.inv(A.T @ A) @ A.T
         print(P)
-        u1 = P @ u0
-        print(u1)
+        # u1 = P @ u0
+        # print(u1)
     else:
         x = np.linalg.inv(A.T @ A) @ A.T @ b
         print(x)
@@ -35,9 +36,36 @@ def orthogonal():
     print(b - (6 / 14) * A)
 
 
+def test_eig():
+    A = np.array([[4, 4],
+                  [-3, 3]])
+    U, D, V = np.linalg.svd(A)
+    # print("U:", U)
+    # print("D:", D)
+    print("V:", V)
+
+
+def test_eig2():
+    A = np.array([[4, 4],
+                  [-3, 3]])
+    D, Q = np.linalg.eig(A.T @ A)
+    print('D:', D)
+    print("Q:", Q)
+    print(1 / np.sqrt(2) * np.array([[1, 1], [-1, 1]]))
+
+
 if __name__ == "__main__":
     # pseudo_A(A=np.array([[1], [2], [3]]), b=np.array([[4], [5], [8]]), u0=np.array([[9], [9], [0]]))
     # pseudo_A(A=np.array([[2], [1], [2]]), b=None, u0=np.array([[9], [9], [0]]))
-    orthogonal()
+    # pseudo_A(A=np.array([[0, 1], [1, 0], [0, 2]]), b=None, u0=np.array([[9], [9], [0]]))
+    # orthogonal()
     # print(np.__version__)
     # print(19/7)
+    from d2l.torch import d2l
+
+    # 2*2*3
+    # A = torch.arange(0, 12).reshape(4, 3)
+    # b = torch.arange(0, 4).reshape(4)
+    # print(d2l.sequence_mask(A, b))
+    test_eig()
+    test_eig2()
